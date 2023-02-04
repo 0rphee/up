d4d2cfb1757d97345ed827917f08e12eaeaefd06
0rphee <0rph3e@proton.me>
2023-02-03 20:41:15 -0600

    pa sesion-3

---
+++ pa/sesion-3/parImpar.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+void mPrint(string text){
+  cout << text << endl;
+}
+
+int main(){
+  int num {0};
+  mPrint("Ingrese un número entero");
+  cin >> num;
+
+  if (num % 2 == 0){
+    mPrint("El número es par");
+  } else {
+    mPrint("El número es impar");
+  }
+  
+  return 0;
+}
+
+
