123d3b516027f6aefd75844c38e710cd722f7bc1
0rphee <0rph3e@proton.me>
2023-02-16 17:51:13 -0600

    sesion-8 pa

---
+++ pa/sesion-8/lib.h
@@ -0,0 +1,26 @@
+/*
+hacer una biblioteca que contenga la función equalsLength, recibe como parámetro dos arreglos y retorna true
+si las cosa cadenas contenidas son del mismo tamaño. En caso contrario retorna un false
+*/
+
+
+#include <iostream>
+
+namespace lib{
+
+    inline bool equalsLength(char arr1[], char arr2[]){
+        int i {0};
+
+        while (true){
+            if (arr1[i] == '\0' || arr2[i] == '\0' ){
+                if (arr1[i] == arr2[i]){
+                    return true;
+                }
+                return false;
+            }
+            i++;
+        } 
+    }
+ 
+}
+
---
+++ pa/sesion-8/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+#include "lib.h"
+
+void testStrings(int testNum, char x[], char y[]){
+    std::printf("Test %d:\n\tString 1: %s\n\tString 2: %s\nResult %d\n\n", testNum, x, y, lib::equalsLength(x, y));
+}
+
+
+int main(){
+    char str1[] = "he lo";
+    char str2[] = "hello"; 
+
+    testStrings(1, str1, str2);
+
+    char str3[] = "hello";
+    char str4[] = "Hello World"; 
+
+    testStrings(2, str3, str4);
+    return 0;
+}
