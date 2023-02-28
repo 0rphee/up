80cd83effb62f36341eed3e810f93abe2f9390b3
0rphee <0rph3e@proton.me>
2023-02-27 20:24:32 -0600

    sesion-8

---
+++ poo/sesion-8/combinacion.py
@@ -0,0 +1,21 @@
+
+def celsiusAFarenheit(celsius: float) -> float: 
+    return (celsius * 9/5) + 32
+
+
+def printBeffo(usuario: str, beffo: str, *amigos: [str]):
+    print(f"""
+Hola {usuario}
+Tu mejor amigo es {beffo}
+Y tus demás amigos son: """)
+    for amigo in amigos:
+        print(f"\t{amigo}")
+
+
+def main():
+    print(celsiusAFarenheit(95))
+    printBeffo("ro", "pancho", "panfleto", "pedrito")
+
+if __name__ == "__main__":
+    main()
+
