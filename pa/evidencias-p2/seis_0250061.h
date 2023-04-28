b88f6958c464df9f47f5ff056c7c388e2eed1d7e
0rphee <0rph3e@proton.me>
2023-04-28 11:06:07 -0600

    correcciones y adicion de faltantes

--- pa/evidencias-p2/seis_0250061.h
+++ pa/evidencias-p2/seis_0250061.h
@@ -58,6 +58,7 @@ namespace seis{
 
         delete[] zoo->animales;
         delete zoo;
+        return 0;
     }
 }
 
--- pa/evidencias-p2/trece_0250061.h
+++ pa/evidencias-p2/trece_0250061.h
@@ -6,34 +6,17 @@
 // Introducción a uniones 
 
 namespace trece{
-
-union Chars {
-    char chars[17] = "abcdefghijklmnop";
-    int ints[4];
-};
-
-int main(){
-    Chars miUnion;
-
-    // printf("Ingrese 16 caracteres\n");
-    // std::cin >> miUnion.chars;
-
-    for (int i {0}; i < 4; i++){
-        char primer  = (miUnion.ints[i] >> (8*0)) & 0xff;
-        char segundo = (miUnion.ints[i] >> (8*1)) & 0xff;
-        char tercero = (miUnion.ints[i] >> (8*2)) & 0xff;
-        char cuarto  = (miUnion.ints[i] >> (8*3)) & 0xff;
-
-        std::cout << primer << segundo << tercero << cuarto << " = " << miUnion.ints[i] <<std::endl;
+    union Datos {
+        int numeroUno;
+        int numeroDos;
+    };
+    int main(){
+        Datos datos;
+        datos.numeroUno = 10;
+        std::cout << "numero guardado: " << datos.numeroDos << std::endl;
+        return 0;
     }
 
-    for (int i {0}; i < 16; i++){
-        ;
-        std::cout << miUnion.chars[i] << " = " << std::bitset<8> (miUnion.chars[i]) <<std::endl;
-    }
-   
-    return 0;
-}
 
 
 }
