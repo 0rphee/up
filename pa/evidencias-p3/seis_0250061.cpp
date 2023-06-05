d56e8c417f61c906f4e8b8a8b07ebd7f308354eb
0rphee <0rph3e@proton.me>
2023-06-05 14:41:04 -0600

    evidencias-p3

--- pa/evidencias-p3/seis_0250061.cpp
+++ pa/evidencias-p3/seis_0250061.cpp
@@ -2,7 +2,25 @@
 
 #include <iostream>
 #include "Animal_0250061.h"
+#include "Rana_0250061.h"
+#include "Pez_0250061.h"
+#include "Perro_0250061.h"
 
 int main(){
-    return 0;
+    // Creación de objetos
+    Perro perro(4, 2, "GWAUWWWW", "mamifero", "cafe", "labrador");
+    Pez pez(0, 2, "glooo", "pez", "azul", true, 2);
+    Rana rana(4, 2, "croook", "anfibio", "verde", true);
+    // Llamada a métodos de las clases derivadas
+    perro.correr();
+    pez.nadar();
+    rana.saltar();
+    perro.sonido();
+    pez.sonido();
+    rana.sonido();
+
+    // Llamada a métodos de la clase base
+    perro.setPatas(3);
+    perro.setOjos(2);
+    perro.setSonido("wooof");
 }
