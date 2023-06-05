906c6010654ac95767b1e3bec1d99823876ce1c3
0rphee <0rph3e@proton.me>
2023-06-04 22:44:35 -0600

    corregir Animal.h y Perro.h

--- pa/evidencias-p3/Perro.h
+++ pa/evidencias-p3/Perro.h
@@ -12,6 +12,12 @@ class Perro : public Animal {
     Perro();
     Perro(short numPatas, short numOjos, std::string sonido_r, std::string tipo, std::string color, std::string raza);
     ~Perro();
+
+    void setPatas(short patas);
+    void setOjos(short ojos);
+    void setSonido(std::string sonido);
+    void setColor(std::string color);
+    void setTipo(std::string Tipo);
 };
 Perro::Perro(){
     this->raza = "indefinido";
@@ -29,5 +35,25 @@ void Perro::sonido(){
 
 void Perro::correr(){
     std::cout << "Perro corre\n";
+}
+
+void Perro::setPatas(short patas){
+    this->numPatas = patas;
+}
+
+void Perro::setOjos(short ojos){
+    this->numOjos = ojos;
+}
+
+void Perro::setSonido(std::string sonido){
+    this->sonido_r = sonido;
+}
+
+void Perro::setColor(std::string color){
+    this->color = color;
+}
+
+void Perro::setTipo(std::string tipo){
+    this->tipo= tipo;
 }
 
