4bad1a14b246f24c52be095777349ecf948dd590
0rphee <0rph3e@proton.me>
2023-08-11 11:40:29 -0600

    ped ejemplo cpp

--- ped/10-09-2023-repaso-c++/Ejemplo.cpp
+++ ped/10-09-2023-repaso-c++/Ejemplo.cpp
@@ -1,1 +1,15 @@
 #include "Ejemplo.h"
+#include <clocale>
+
+Ejemplo::Ejemplo(){} 
+
+Ejemplo::~Ejemplo(){}
+
+void Ejemplo::Mostrar(){
+    cout << "\n\n\n\t\t\tMi primera Línea" << endl;
+    cout << "\n\n\n\t\t\tHola mundo" << endl << endl;
+}
+
+int Ejemplo::Sumar(int x, int y){
+    return x + y;
+}
