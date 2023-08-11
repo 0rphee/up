8f0e9df55914d6b2dde4d0114606b2294b945c13
0rphee <0rph3e@proton.me>
2023-08-11 11:40:29 -0600

    ped ejemplo cpp

--- ped/10-09-2023-repaso-c++/Ejemplo.h
+++ ped/10-09-2023-repaso-c++/Ejemplo.h
@@ -1,3 +1,18 @@
+#pragma once
+#include <iostream>
+
+using namespace std;
+
 class Ejemplo{
+
+public:
+    Ejemplo();
+    ~Ejemplo();
+    void Mostrar();
+    int Sumar(int, int);
+
+private:
+
+protected:
 
 };
--- ped/10-09-2023-repaso-c++/Source.cpp
+++ ped/10-09-2023-repaso-c++/Source.cpp
@@ -1,7 +1,21 @@
+#include "Ejemplo.cpp"
 #include <iostream>
 
-int main(){
-    printf("aaaa\n");
+int main() {
+  Ejemplo ej = Ejemplo();
 
-    return 0;
+  ej.Mostrar();
+
+  int a, b;
+  cout << "Dime un entero" << endl;
+  cin >> a;
+
+  cout << "Dime otro entero" << endl;
+  cin >> b;
+
+  int suma = ej.Sumar(a, b);
+
+  cout << "Resultado:" << suma << endl << endl;
+
+  return 0;
 }
