9e13388ab245222e80152a202379379b35504cc0
0rphee <0rph3e@proton.me>
2024-02-23 23:45:36 -0600

    lpro tablas de verdad, script para generarlas

---
+++ lpro/clase.py
@@ -0,0 +1,9 @@
+from logica import *
+
+def clase(p: bool, q: bool, r: bool):
+    left = p or (not q)
+    right = not r
+    return unidir(left, right)
+
+if __name__ == "__main__":
+    genTabla(clase, "(P or not Q) arrow not R")
