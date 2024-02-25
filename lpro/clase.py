049dc67d3a1189798a555cd2f26c2223f2d75129
0rphee <0rph3e@proton.me>
2024-02-25 16:11:42 -0600

    lpro ajustes scripts python

--- lpro/clase.py
+++ lpro/clase.py
@@ -1,9 +1,11 @@
 from logica import *
 
+
 def clase(p: bool, q: bool, r: bool):
     left = p or (not q)
     right = not r
     return unidir(left, right)
+
 
 if __name__ == "__main__":
     genTabla(clase, "(P or not Q) arrow not R")
