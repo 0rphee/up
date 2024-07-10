11b780b8f47b9c76dfb1e27d80e2671ab70b0455
0rphee <0rph3e@proton.me>
2024-07-09 23:40:07 -0600

    dw tarea/avance proyecto 2
    mio y del profe

---
+++ dw/web/tarea-proy-2/prof/index.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Reporteador</title>
+
+    <!-- BOOTSTRAP -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+    <!-- BOOTSTRAP -->
+
+    <!-- FONT-AWESOME -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css" integrity="sha512-SnH5WK+bZxgPHs44uWIX+LLJAJ9/2PkPKZ5QiAj6Ta86w+fsb2TkcmfRyVX3pBnMFcV7oQPJkl9QevSCWr3W6A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- FONT-AWESOME -->
+
+    <!-- JQUERY -->
+    <script defer src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+    <!-- JQUERY -->
+
+    <script defer src="script.js"></script>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body class="d-flex full-height">
+
+    <h1>Visitas a museo</h1>
+    <div class="">
+        <div class="row mx-0 my-4">
+            <div class="col bg-gold">
+                <table class="mi-tabla">
+                    <tr id="head">
+                        <td id="nac">1234</td>
+                        <td id="ext">4321</td>
+                        <td id="lang1">1</td>
+                        <td id="lang2">2</td>
+                    </tr>
+                    <tr id="">
+                        <td>No. Nacionales</td>
+                        <td>No. Extranjeros</td>
+                        <td>Primera Lengua</td>
+                        <td>Segunda Lengua</td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+    </div>
+
+
+    <div>
+    </div>
+
+
+    
+
+</body>
+
+</html>
