c8c39df6a1e7180481483e201f9a840bffd19ce4
0rphee <0rph3e@proton.me>
2024-07-11 11:48:54 -0600

    dw copia de ejer9 para hacer proyecto 2

---
+++ dw/web/proy-2-reporteador/config.php
@@ -0,0 +1,11 @@
+<?php
+
+$DB_data = array(
+    "server" => "localhost:8889",
+    "connInfo" => array(
+        "UID" => "pantera",
+        "PWD" => "cualquiera",
+    ),
+    "Database" => "panteras",
+);
+?>
