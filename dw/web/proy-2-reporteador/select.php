88d7343d831f368f4da93270a35e04507378e733
0rphee <0rph3e@proton.me>
2024-07-12 12:21:21 -0600

    dw proy-w-reporteador avance
    * faltan estilos
    * falta terminar el resumen

--- dw/web/proy-2-reporteador/select.php
+++ dw/web/proy-2-reporteador/select.php
@@ -2,7 +2,33 @@
 include_once "config.php";
 
 $conn = connectOrDie();
-$query = $_POST["query"];
+
+$filtros = $_POST["filtros"];
+
+$columns = [];
+$whereConditions = [];
+$counter = 0;
+foreach ($filtros as $visitasColName => $v) {
+    // array_push($columns, "visitas.$visitasColName AS '{$v["title"]}'");
+    array_push($columns, "visitas.$visitasColName");
+    if ($v["filtro"] == "true") {
+        if (strlen($v["val"] > 0)) {
+            if ($visitasColName == "estado_escolar") {
+                $condition = "visitas.{$visitasColName} = '{$v['val']}'";
+            } else {
+                $condition = "visitas.{$visitasColName} = {$v['val']}";
+            }
+            array_push($whereConditions, $condition);
+        }
+    } else {
+    }
+}
+$totalColumnString = implode(", ", $columns);
+$totalConditionString = count($whereConditions) > 0 ?  " WHERE " . implode(" AND ", $whereConditions) : "";
+
+$query = "SELECT " . $totalColumnString . " FROM  visitas" . $totalConditionString;
+// echo $query;
+
 $result = $conn->query($query);
 
 $data = [];
