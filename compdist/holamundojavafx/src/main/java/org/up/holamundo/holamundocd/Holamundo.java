2b660e5dd9360f1ee64738139b7e453880a490c3
0rphee <0rph3e@proton.me>
2025-01-29 00:39:03 -0600

    compdist holamundojavafx start

--- compdist/holamundojavafx/src/main/java/org/up/holamundo/holamundocd/Holamundo.java
+++ compdist/holamundojavafx/src/main/java/org/up/holamundo/holamundocd/Holamundo.java
@@ -4,13 +4,23 @@
 
 package org.up.holamundo.holamundocd;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 /**
  *
  * @author roger
  */
 public class Holamundo {
+    private static final Logger LOGGER = LogManager.getLogger(Holamundo.class);
 
+    /**
+     * Es el metodo principal
+     * 
+     * @param args 
+    **/
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        // System.out.println("Hello World!");
+	LOGGER.info("Hello world logger");
     }
 }
