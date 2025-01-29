9fea597924e078a54a86c8557f9c03b44ee1eb45
0rphee <0rph3e@proton.me>
2025-01-29 00:39:03 -0600

    compdist holamundojavafx start

---
+++ compdist/holamundojavafx/src/main/java/org/up/holamundo/holamundocd/PleaseProvideControllerClassName.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.up.holamundo.holamundocd;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+/**
+ *
+ * @author roger
+ */
+public class PleaseProvideControllerClassName {
+
+	@FXML
+	private Button presionameButton;
+	
+}
