04890a0ed917b4b4ae483d0d1f6a46a111434f18
0rphee <0rph3e@proton.me>
2025-02-17 18:37:27 -0600

    dam clase statebinding

---
+++ dam/statebinding/statebinding/statebindingApp.swift
@@ -0,0 +1,17 @@
+//
+//  statebindingApp.swift
+//  statebinding
+//
+//  Created by roger on 17/02/25.
+//
+
+import SwiftUI
+
+@main
+struct statebindingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
