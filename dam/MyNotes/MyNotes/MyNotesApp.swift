df3876d1bc165909c8e46b5fe6c74f0f0751decb
0rphee <0rph3e@proton.me>
2025-03-21 18:24:11 -0600

    dam MyNotes

---
+++ dam/MyNotes/MyNotes/MyNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyNotesApp.swift
+//  MyNotes
+//
+//  Created by roger on 28/02/25.
+//
+
+import SwiftUI
+
+@main
+struct MyNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
